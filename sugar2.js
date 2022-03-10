e9760baf50386881fcdad98dc2e438a8035ea011
JonHuang <s72503013@gmail.com>
2022-03-10 23:51:35 +0800

    update

--- sugar2.js
+++ sugar2.js
@@ -1,7 +1,16 @@
-const sentence = 'The quick brown fox jumps over the lazy dog.';
+const sentence = 'The quick brown fox jum';
 
 const index = 4;
 
 console.log(`The character code ${sentence.charCodeAt(index)} is equal to ${sentence.charAt(index)}`);
 // expected output: "The character code 113 is equal to q"
 
+
+// console.log(sentence.charCodeAt(6));
+
+let sent = ["a", "b", "c", "d", "f", "g"];
+const Sent = sent.toString()
+console.log(Sent.charCodeAt(0));
+console.log(Sent.charCodeAt(2));
+
+console.log();
